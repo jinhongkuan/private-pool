9f564e0c91bc39d05a19c97bb822db33e9043d33
Jacob Aune <jacobaune@Jacobs-MacBook-Air.local>
2022-04-01 11:34:36 -0400

    jacob first commit

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -9,7 +9,7 @@ contract YourContract {
 
   event SetPurpose(address sender, string purpose);
 
-  string public purpose = "Building Unstoppable Apps!!!";
+  string public purpose = "Jacob";
 
   constructor() payable {
     // what should we do on deploy?
